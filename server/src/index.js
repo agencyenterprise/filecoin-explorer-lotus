60588050afcf9f91f126fa5aa910dc7df9845f70
Gabriel Siedler <gabrielsiedler@gmail.com>
2020-01-22 16:16:51 -0300

    waiting for connection before starting server

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,22 @@
-import { app } from './app';
-import { logger } from './services/logger';
+import { app } from "./app";
+import { logger } from "./services/logger";
+import { connect } from "./services/db";
 
 const port = process.env.PORT || 8888;
-const host = process.env.HOST || 'localhost';
+const host = process.env.HOST || "localhost";
 
 const startServer = () => {
   app.listen(+port, host, () => {
-    logger.info(`Server started at [ http://${host}:${port} ]`)
-    logger.info(`Environment ${process.pid}: ${process.env.NODE_ENV}`)
-  })
-}
+    logger.info(`Server started at [ http://${host}:${port} ]`);
+    logger.info(`Environment ${process.pid}: ${process.env.NODE_ENV}`);
+  });
+};
 
-startServer()
+(async () => {
+  console.log("Connecting to database...");
+
+  await connect();
+
+  console.log("Starting server...");
+  startServer();
+})();
--- server/src/services/db/db.js
+++ server/src/services/db/db.js
@@ -2,10 +2,14 @@ import { Client } from "pg";
 
 const dbParams = {
   connectionString: process.env.DATABASE_URL
-}
+};
+
+console.log(dbParams.connectionString);
 
 export const db = new Client(dbParams);
 
-export const connect = () => {
-  db.connect();
-}
+export const connect = () =>
+  new Promise(resolve => {
+    console.log("Connected to database.");
+    db.connect(resolve);
+  });
