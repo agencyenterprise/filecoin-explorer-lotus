6f69a2f1a9cce6b1c6eb2b87701afce426182745
Gabriel Siedler <gabrielsiedler@gmail.com>
2020-01-25 18:24:23 -0300

    fixing error handling on db connection

--- server/src/services/db/db.js
+++ server/src/services/db/db.js
@@ -9,7 +9,14 @@ const dbParams = {
 export const db = new Client(dbParams)
 
 export const connect = () =>
-  new Promise((resolve) => {
-    chalk.success(`Connected to database at ${dbParams.connectionString}`)
-    db.connect(resolve)
+  new Promise((resolve, reject) => {
+    db.connect((error) => {
+      if (error) {
+        chalk.error(`error connecting to the database ${error}`, error)
+        reject(error)
+      }
+
+      chalk.success(`Connected to database at ${dbParams.connectionString}`)
+      resolve()
+    })
   })
