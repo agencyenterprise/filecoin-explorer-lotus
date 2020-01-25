81ea87394b44761d14ea3b6f915b8548cc335095
Gabriel Siedler <gabrielsiedler@gmail.com>
2020-01-25 18:07:33 -0300

    debug

--- server/src/routes/blocks/getBlockRange.js
+++ server/src/routes/blocks/getBlockRange.js
@@ -2,6 +2,13 @@ import { getBlockRange as getBlockRangeData } from '../../services/db/blocks'
 
 export const getBlockRange = async (req, res) => {
   console.log('getting block range data')
-  const blockRange = await getBlockRangeData()
+
+  let blockRange
+  try {
+    blockRange = await getBlockRangeData()
+  } catch (e) {
+    console.log('Error on query', e)
+  }
+
   res.json(blockRange)
 }
