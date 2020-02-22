3c998983b9e2987620f831790381a3265d0a2365
Gabriel Siedler <gabrielsiedler@gmail.com>
2020-02-21 21:20:47 -0300

    adding height

--- client/public/dcgraph/js/app_layout.js
+++
@@ -1,4 +0,0 @@
-// customization point for the main test page
-// constraints and types keyed by the `applayout` query string
-var app_layouts = {};
-
--- client/public/dcgraph/js/index.js
+++
@@ -1,8 +0,0 @@
-d3.select('img.animate').on({
-    mouseenter: function(d) {
-        this.src = "img/screenshots/" + this.id + ".gif";
-    },
-    mouseleave: function(d) {
-        this.src = "img/screenshots/" + this.id + ".png";
-    }
-});
